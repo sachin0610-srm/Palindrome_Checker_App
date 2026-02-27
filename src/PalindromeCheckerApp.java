2bf81641b291ef6951de5700dfec3fedc7421ea0
sachin006-srm <sachinlava14@gmail.com>
2026-02-27 14:44:07 +0530

    UC1:Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,6 @@
+import java.util.*;
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System\nDESCRIPTION: to check whether the given input is Palindrome or not.\nAPP INITIALIZED SUCCESSFULLY.");
+    }
 }
