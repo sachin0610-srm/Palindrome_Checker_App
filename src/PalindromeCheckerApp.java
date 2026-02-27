6f719d150a3049bd6ea0220b054a8cb62b332ccd
sachin006-srm <sachinlava14@gmail.com>
2026-02-27 15:54:11 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,31 @@
 import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("Enter a string: ");
-            String input = sc.nextLine();
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-            if (isPalindrome(input)) {
-                System.out.println("It is a palindrome.");
-            } else {
-                System.out.println("It is not a palindrome.");
-            }
-
+        if (isPalindrome(input)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
         }
+    }
     public static boolean isPalindrome(String text) {
-        String reversed = new StringBuilder(text).reverse().toString();
-        return text.equals(reversed);
+        char[] arr = text.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
